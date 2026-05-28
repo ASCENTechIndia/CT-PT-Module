d53b85abc7c619874d7bd09bb4d5546d75cb3657
Swati-Ascentech <swati.gupta@ascentechindia.com>
2026-05-28 12:33:58 +0530

    validation added

--- CT-PT API/src/modules/registerComplaint/registerComplaint.routes.js
+++ CT-PT API/src/modules/registerComplaint/registerComplaint.routes.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const validate = require('../../middleware/validate.middleware');
 const { authRequired } = require('../../middleware/auth');
-// const { } = require('./Reports.validation');
-const { getWardList, getToiletList, getComplaintTypeList,registerComplaint } = require('./registerComplaint.controller');
+const { complaintRegistrationSchema } = require('./registerComplaint.validation');
+const { getWardList, getToiletList, getComplaintTypeList, registerComplaint } = require('./registerComplaint.controller');
 
 const router = express.Router();
 
 router.get('/wardList', getWardList);
 router.get('/toiletList', getToiletList);
 router.get('/complaintTypeList', getComplaintTypeList);
-router.post('/insertComplaint', registerComplaint );
+router.post('/insertComplaint', validate(complaintRegistrationSchema), registerComplaint );
 
 
 module.exports = router;
