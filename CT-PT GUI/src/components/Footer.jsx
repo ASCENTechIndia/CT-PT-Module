03586a36dbf19fa7d3f4ec935fe486db72e1e376
Pingcodeping <Pingcodeping@gmail.com>
2026-05-28 18:11:17 +0530

    Modify Footer.jsx

--- CT-PT GUI/src/components/Footer.jsx
+++ CT-PT GUI/src/components/Footer.jsx
@@ -2,9 +2,13 @@ export default function Footer() {
   return (
     <footer className="admin-footer">
       <div className="container-fluid px-3 px-lg-4">
-        <span>Copyright 2026 adminHMD. <br /> Developed by <a target="_blank" rel="noreferrer" className="fw-bold text-success" href="https://github.com/HasanMahmudDev">Md. Hasan Mahmud</a> • Distributed by <a target="_blank" rel="noreferrer" className="fw-bold text-success" href="https://themewagon.com">ThemeWagon</a> </span>
-        <span>Professional dashboard template.</span>
-        <span>React admin dashboard.</span>
+        <span className="m-auto">Copyright {new Date().getFullYear()} AscenTech.
+          {/* <br /> 
+          • Distributed by <a target="_blank" rel="noreferrer" className="fw-bold text-success" href="https://themewagon.com">ThemeWagon</a> 
+          Developed by <a target="_blank" rel="noreferrer" className="fw-bold text-success" href="https://github.com/HasanMahmudDev">Md. Hasan Mahmud</a>  */}
+          </span>
+        {/* <span>Professional dashboard template.</span>
+        <span>React admin dashboard.</span> */}
       </div>
     </footer>
   );
